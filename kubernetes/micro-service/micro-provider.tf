ade129d992e535e821d960c05526f2b9fd09f954
Opebiodun <opebiodun@gmail.com>
2026-07-20 23:21:05 +0100

    Fix EKS Kubernetes provider data sources

--- kubernetes/micro-service/micro-provider.tf
+++ kubernetes/micro-service/micro-provider.tf
@@ -15,17 +15,6 @@ data "aws_eks_cluster_auth" "micro-dev-eks-demo" {
   name = "micro-dev-eks-demo"
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.micro-dev-eks-demo.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.micro-dev-eks-demo.certificate_authority[0].data)
-  version          = "2.16.1"
-  config_path = "~/.kube/config"
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", "micro-dev-eks-demo"]
-    command     = "aws"
-  }
-}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.micro-dev-eks-demo.endpoint
