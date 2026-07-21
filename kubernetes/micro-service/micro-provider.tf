9f72b6f110b93109f778f92ee1ff7ebb54a944b7
Opebiodun <opebiodun@gmail.com>
2026-07-21 02:10:12 +0100

    Configure Kubernetes and Helm providers for EKS

--- kubernetes/micro-service/micro-provider.tf
+++ kubernetes/micro-service/micro-provider.tf
@@ -15,11 +15,20 @@ data "aws_eks_cluster_auth" "micro-dev-eks-demo" {
   name = "micro-dev-eks-demo"
 }
 
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.micro-dev-eks-demo.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.micro-dev-eks-demo.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.micro-dev-eks-demo.token
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.micro-dev-eks-demo.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.micro-dev-eks-demo.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.micro-dev-eks-demo.token
+  }
+}
 
+provider "aws" {
+  region = "eu-west-2"
+}
--- kubernetes/nginx-controller/providers-nginx.tf
+++ kubernetes/nginx-controller/providers-nginx.tf
@@ -15,9 +15,10 @@ data "aws_eks_cluster_auth" "micro-dev-eks-demo" {
   name = "micro-dev-eks-demo"
 }
 
-
-provider "aws" {
-  region     = "eu-west-2"
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.micro-dev-eks-demo.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.micro-dev-eks-demo.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.micro-dev-eks-demo.token
 }
 
 provider "helm" {
@@ -27,6 +28,11 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.micro-dev-eks-demo.token
   }
 }
+
+provider "aws" {
+  region     = "eu-west-2"
+}
+
 #export the kubeconfig file
 
 #export KUBECONFIG=~/.kube/config
