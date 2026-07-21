1052e17caeb46d3afb1008d05af13c77fb129962
Opebiodun <opebiodun@gmail.com>
2026-07-21 21:55:17 +0100

    Configure Kubernetes and Helm providers for EKS

--- kubernetes/micro-service/micro-provider.tf
+++ kubernetes/micro-service/micro-provider.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {}
 
-
-terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -14,30 +12,23 @@ terraform {
 data "aws_eks_cluster" "micro-dev-eks-demo" {
   name = "micro-dev-eks-demo"
 }
-
-data "aws_eks_cluster_auth" "micro-dev-eks-demo" {
+data "aws_eks_cluster_auth" "micro-dev-eks-demo_auth" {
   name = "micro-dev-eks-demo"
 }
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.micro-dev-eks-demo.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.micro-dev-eks-demo.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.micro-dev-eks-demo.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.micro-dev-eks-demo.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.micro-dev-eks-demo.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.micro-dev-eks-demo.token
+  version          = "2.16.1"
+  config_path = "~/.kube/config"
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", "micro-dev-eks-demo"]
+    command     = "aws"
   }
 }
 
-provider "aws" {
-  region=$AWS_REGION
-  input=false
+resource "kubernetes_namespace" "kube-namespace" {
+  metadata {
+    name = "sock-shop"
+  }
 }
-
-#provider "aws" {
-#  region = "eu-west-2"
-#}
